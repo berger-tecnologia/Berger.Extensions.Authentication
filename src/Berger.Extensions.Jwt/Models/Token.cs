9a10521ec86610393f9767c43a79103a199c3a80
Rafael Cronemberger <rafael@berger.com.br>
2024-11-11 16:22:48 -0300

    Package updates

--- src/Berger.Extensions.Jwt/Models/Token.cs
+++ src/Berger.Extensions.Jwt/Models/Token.cs
@@ -4,9 +4,7 @@ namespace Berger.Extensions.Jwt
 {
     public class Token : BaseEntity
     {
-        public Guid EnvironmentId { get; set; }
-        public Guid EntityTypeId { get; set; }
-        public Guid? EntityId { get; set; }
+        public Guid ApplicationId { get; set; }
         public string Issuer { get; set; } = string.Empty;
         public string AccessToken { get; set; } = string.Empty;
         public string RefreshToken { get; set; } = string.Empty;
