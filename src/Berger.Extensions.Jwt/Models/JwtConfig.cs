3d521922b868c148c45fce134f369b41240a807c
Rafael Cronemberger <rafael@berger.com.br>
2024-12-01 14:52:00 -0300

    Package updates

---
+++ src/Berger.Extensions.Jwt/Models/JwtConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Berger.Extensions.Jwt
+{
+    public class JwtConfig
+    {
+        public int Expiration { get; init; } = 0;
+        public string Issuer { get; set; }
+        public string Secret { get; set; }
+        public string Audience { get; set; }
+        public int RefreshTokenExpiration { get; init; } = 0;
+    }
+}
